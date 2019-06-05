254fc087d79a89dbd46c34c901b39f8dae344c32
elanrosen <elan.rosen@gmail.com>
2019-06-05 11:49:23 -0400

    f
    f

--- edge.py
+++ edge.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 email = 'surfer.zoke@gmail.com' ##inputs for account
 password = 'Burrito1'
@@ -21,7 +22,7 @@ def setting():
 
     profile_counts = browser.find_elements_by_class_name('list_count')
     question_count = int(profile_counts[1].text)
-    for i in range(5, question_count):
+    for i in range(1, 5):#normally question_count
 
         questions = browser.find_elements_by_class_name('ui_content_title ui_content_title--default ui_content_title--medium')
         while i >= len(questions):
@@ -32,13 +33,16 @@ def setting():
         questions[(i)].click()
         questionPage = browser.window_handles[1] ##changes browser tab
         browser.switch_to.window(questionPage)
-        request = browser.find_element_by_id('request')
-        request.click()
         requesting()
         browser.close()
         browser.switch_to.window(browser.window_handles[0]) # heads back to question page
 
 def requesting():
+    request = browser.find_elements_by_id('request') #Even though there is only one requets element will be present, array is returned so that it can be counted to determine whether request is present. Selenium does not allow to check for single element without returning error
+    while (len(request)!=0): #runs loop as long as request is still visible
+        request[0].click()
+        request = browser.find_elements_by_id('request')
+    time.sleep(.3)
     buttons = browser.find_elements_by_class_name('u-flex-none u-relative u-flex-align-self--center button_wrapper')
     users = browser.find_elements_by_class_name('user')  # to look cool shows names and stuff
     buttoncount = len(buttons)
