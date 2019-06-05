3227a6944fdaeca55124ed3157d3978b5b40aabb
elanrosen <elan.rosen@gmail.com>
2019-06-04 21:25:28 -0400

    Initial commit

---
+++ dog.py
@@ -0,0 +1,9 @@
+x = 10
+i = 0
+while (i < x):
+    print(x)
+    x = 5
+    i += 1
+
+for x in range(0,10):
+    print(x)
---
+++ test.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+
+email = 'surfer.zoke@gmail.com' ##inputs for account
+password = 'Burrito1'
+browser = webdriver.Edge("MicrosoftWebDriver.exe") ##loads driver
+browser.implicitly_wait(5)
+
+def login():
+    browser.get('http://www.quora.com')
+
+    fields = browser.find_elements_by_class_name("text header_login_text_box ignore_interaction")  # selects the login text fields
+    fields[0].send_keys(email)
+    fields[1].send_keys(password)
+
+    login = browser.find_element_by_class_name('submit_button ignore_interaction')
+    login.click()  # logs in
+
+def setting_up_request():
+    browser.get('https://www.quora.com/profile/Tom-Pierre-1/questions')  # heads to question page
+
+    profile_counts = browser.find_elements_by_class_name('list_count')
+    question_count = int(profile_counts[1].text)
+    for i in range(0, question_count):
+        '''if i==0:
+            questions[(i)].click()
+        else:
+            questions = browser.find_elements_by_class_name('ui_content_title ui_content_title--default ui_content_title--medium')  ##adds new questions to list
+            browser.execute_script("arguments[0].scrollIntoView(true);", questions[(i - 1)])
+            time.sleep(.5)
+            questions = browser.find_elements_by_class_name('ui_content_title ui_content_title--default ui_content_title--medium')  ##adds new questions to list
+            print(questions[i].text)
+            questions[(i)].click()'''
+        questions = browser.find_elements_by_class_name('ui_content_title ui_content_title--default ui_content_title--medium')
+        while i >= len(questions):
+            browser.execute_script('window.scrollBy(0, 110)')
+            questions = browser.find_elements_by_class_name('ui_content_title ui_content_title--default ui_content_title--medium')
+        print(questions[i].text, i)
+
+        questions[(i)].click()
+        questionPage = browser.window_handles[1] ##changes browser tab
+        browser.close()
+        browser.switch_to.window(questionPage)
+
+        '''request = browser.find_element_by_id('request')
+        request.click()
+        time.sleep(1)'''
+        browser.get('https://www.quora.com/profile/Tom-Pierre-1/questions')  # heads to question page
+
+
+def requesting():
+    buttons = browser.find_elements_by_class_name('u-flex-none u-relative u-flex-align-self--center button_wrapper')
+    users = browser.find_elements_by_class_name('user')  # to look cool shows names and stuff
+    buttoncount = len(buttons)
+    if buttoncount > 50:
+        buttoncount = 50
+
+    for x in range(0, buttoncount):
+        browser.execute_script("arguments[0].scrollIntoView(true);", users[(x)])
+        buttons[x].click()  ##must be even for whatever reason
+        print('Requested ' + users[x].text)
+        x += 2
+
+def submit():
+    submit = browser.find_elements_by_class_name('submit_button modal_action')
+    submit[2].click()
+
+login()
+setting_up_request()
+requesting()
+
+
+
+
+
